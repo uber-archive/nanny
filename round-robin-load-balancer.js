2d1703d0dc5a998ef708d6c87b91a232440fbe53
Kris Kowal <kris@uber.com>
2014-10-17 14:01:53 -0700

    Fix bug in load balancer server restarting
    Should use requested address, in particular, instead of last given.

--- round-robin-load-balancer.js
+++ round-robin-load-balancer.js
@@ -111,7 +111,7 @@ RoundRobinLoadBalancer.prototype.start = function () {
         this.logger.debug('listening for connections on behalf of workers', {
             port: this.port
         });
-        this.server.listen(this.port || 0, this.address, this.backlog);
+        this.server.listen(this.port || 0, this.requestedAddress, this.requestedBacklog);
 
     } else if (this.state === 'stopping') {
         this.restart = true;
